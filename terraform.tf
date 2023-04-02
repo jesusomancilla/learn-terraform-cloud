06eef7a87518a9657a28d9252989590f47b23c11
Mancilla <jesus.mancilla@accenture.com>
2023-04-01 19:53:23 -0600

    remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "accn"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+  */
 
   required_providers {
     aws = {
